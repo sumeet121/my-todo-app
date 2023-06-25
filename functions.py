e52f2fb0b6bd7a8cb2c98e752031b6ca19bb1f30
Sumeet <sumeetp121@gmail.com>
2023-06-25 18:05:30 +0530

    initial commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+FILEPATH = "D:/Ardit_Python/app1\journal/todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return the list of to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write the to-do items list in text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
